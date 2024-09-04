a835dc60bf9cb025bf0864680b53aec27f93a3d5
Bloogefest <bloogefest@gmail.com>
2024-09-05 00:45:21 +0300

    Изменение модуля `com.sogeor.validation`

--- validation/src/main/java/module-info.java
+++ validation/src/main/java/module-info.java
@@ -5,12 +5,12 @@
  * @since 1.0.0-RC1
  */
 module com.sogeor.validation {
-    requires org.jetbrains.annotations;
     requires com.sogeor.throwable;
+    requires org.jetbrains.annotations;
 
-    exports com.sogeor.validation;
     exports com.sogeor.validation.argument;
     exports com.sogeor.validation.condition;
     exports com.sogeor.validation.field;
     exports com.sogeor.validation.variable;
+    exports com.sogeor.validation;
 }
