b51a21f116eb6bc5e1a1435f2a4664164b6c9e45
Bloogefest <bloogefest@gmail.com>
2024-09-06 21:45:24 +0300

    Изменение пакета `com.sogeor.common.singleton`

--- common/src/main/java/com/sogeor/common/singleton/package-info.java
+++ common/src/main/java/com/sogeor/common/singleton/package-info.java
@@ -2,6 +2,10 @@
  * Представляет собой пакет для работы с классами, которые спроектированы согласно порождающему шаблону проектирования —
  * одиночке.
  *
+ * @see com.sogeor.common
+ * @see com.sogeor.common.singleton.SingletonCreationFault
+ * @see com.sogeor.common.singleton.SingletonFault
+ * @see com.sogeor.common.singleton.SingletonInstanceFault
  * @since 1.0.0-RC1
  */
 package com.sogeor.common.singleton;
