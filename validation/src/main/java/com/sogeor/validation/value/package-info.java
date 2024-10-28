617bc6df9b1ad82378bd839c09c1a7e260a49f91
Bloogefest <bloogefest@gmail.com>
2024-10-28 23:15:29 +0300

    Изменение модуля `com.sogeor.validation`

--- validation/src/main/java/com/sogeor/validation/value/package-info.java
+++ validation/src/main/java/com/sogeor/validation/value/package-info.java
@@ -2,6 +2,8 @@
  * Представляет собой пакет для валидации значений.
  *
  * @see com.sogeor.validation
+ * @see com.sogeor.validation.value.NonNullValueValidationFault
+ * @see com.sogeor.validation.value.NullValueValidationFault
  * @see com.sogeor.validation.value.ValueValidationFault
  * @since 1.0.0-RC1
  */
