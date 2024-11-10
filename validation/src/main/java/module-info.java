647fe93f35b7d6e905605fd17be7264d541a8543
Bloogefest <bloogefest@gmail.com>
2024-11-11 00:48:41 +0300

    Изменение модуля `com.sogeor.validation`

--- validation/src/main/java/module-info.java
+++ validation/src/main/java/module-info.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2024 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
- * Представляет собой модуль для валидации аргументов, условий, полей, значений и переменных.
+ * Представляет собой модуль для валидации аргументов методов и конструкторов, истинности и ложности условий, значения
+ * полей, анонимных значений, а также значений переменных.
  *
  * @see com.sogeor.validation
  * @since 1.0.0-RC1
