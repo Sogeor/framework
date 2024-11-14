4e5fd78ef7dd3bced889bd30698e04ee3b8eaa06
Bloogefest <bloogefest@gmail.com>
2024-11-14 21:01:15 +0300

    Изменение неисправности `NonNullValueValidationFault`

--- validation/src/main/java/com/sogeor/validation/value/NonNullValueValidationFault.java
+++ validation/src/main/java/com/sogeor/validation/value/NonNullValueValidationFault.java
@@ -21,8 +21,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Представляет собой непроверяемую неисправность программы, возникающую из-за ошибки валидации ненулевого значения, то
- * есть значения, которое должно быть нулевым, но таковым является.
+ * Представляет собой непроверяемую программную неисправность, возникающую при неудачной валидации анонимного значения,
+ * которое должно быть нулевым, но таковым не является.
  *
  * @since 1.0.0-RC1
  */
@@ -33,7 +33,7 @@ public class NonNullValueValidationFault extends ValueValidationFault {
      *
      * @since 1.0.0-RC1
      */
-    public static final @NotNull String DEFAULT_MESSAGE = "The passed value must be null";
+    public static final @NotNull String DEFAULT_MESSAGE = "The anonymous value must be null";
 
     /**
      * Представляет собой шаблонное сообщение.
