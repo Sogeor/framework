6b52c67d5e112509c7b3f9a76022119960341663
Bloogefest <bloogefest@gmail.com>
2024-11-14 21:05:45 +0300

    Изменение пакета `com.sogeor.validation`

--- validation/src/main/java/com/sogeor/validation/package-info.java
+++ validation/src/main/java/com/sogeor/validation/package-info.java
@@ -21,6 +21,7 @@
  * @see com.sogeor.validation.argument
  * @see com.sogeor.validation.condition
  * @see com.sogeor.validation.field
+ * @see com.sogeor.validation.value
  * @see com.sogeor.validation.variable
  * @see com.sogeor.validation.ValidationFault
  * @since 1.0.0-RC1
