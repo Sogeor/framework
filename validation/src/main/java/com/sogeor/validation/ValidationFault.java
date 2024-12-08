2889ed6f1a5e63c27605a1f0f1545a6ee7c0592d
George <bloogefest@gmail.com>
2024-12-08 15:06:24 +0300

    Изменение неисправности `ValidationFault`

--- validation/src/main/java/com/sogeor/validation/ValidationFault.java
+++ validation/src/main/java/com/sogeor/validation/ValidationFault.java
@@ -16,95 +16,103 @@
 
 package com.sogeor.validation;
 
+import com.sogeor.annotation.Contract;
+import com.sogeor.annotation.NonNull;
+import com.sogeor.annotation.Nullable;
 import com.sogeor.throwable.fault.UncheckedFault;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
- * Представляет собой непроверяемую программную неисправность, возникающую при неудачной валидации.
+ * Представляет собой непроверяемую программную неисправность, связанную с неудачной валидацией.
  *
  * @since 1.0.0-RC1
  */
 public class ValidationFault extends UncheckedFault {
 
     /**
-     * Представляет собой сообщение по умолчанию.
+     * Содержит сообщение по умолчанию.
      *
      * @since 1.0.0-RC1
      */
-    public static final @NotNull String DEFAULT_MESSAGE = "Failed to validate";
+    public static final @NonNull String DEFAULT_MESSAGE = "Failed to validate";
 
     /**
-     * Представляет собой шаблонное сообщение.
+     * Содержит шаблонное сообщение.
      *
      * @since 1.0.0-RC1
      */
-    public static final @NotNull String TEMPLATE_MESSAGE = "Failed to validate %s";
+    public static final @NonNull String TEMPLATE_MESSAGE = "Failed to validate %s";
 
     /**
-     * Представляет собой конструктор по умолчанию.
+     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, {@linkplain #DEFAULT_CAUSE причиной возникновения},
+     * параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по
+     * умолчанию.
      *
      * @since 1.0.0-RC1
      */
-    @Contract(pure = true)
+    @Contract("-> new")
     public ValidationFault() {
         super(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
     }
 
     /**
-     * Представляет собой конструктор, позволяющий задать сообщение.
+     * Создаёт экземпляр с {@linkplain #DEFAULT_CAUSE причиной возникновения}, параметрами
+     * {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию,
+     * а также с переданным сообщением.
      *
      * @param message сообщение.
      *
      * @since 1.0.0-RC1
      */
-    @Contract(pure = true)
+    @Contract("? -> new")
     public ValidationFault(final @Nullable String message) {
         super(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
     }
 
     /**
-     * Представляет собой конструктор, позволяющий задать причину возникновения.
+     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, параметрами
+     * {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию,
+     * а также с переданной причиной возникновения.
      *
      * @param cause причина возникновения.
      *
      * @since 1.0.0-RC1
      */
-    @Contract(pure = true)
+    @Contract("? -> new")
     public ValidationFault(final @Nullable Throwable cause) {
         super(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
     }
 
     /**
-     * Представляет собой конструктор, позволяющий задать сообщение и причину возникновения.
+     * Создаёт экземпляр с параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и
+     * {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию, а также с переданным сообщением и причиной
+     * возникновения.
      *
      * @param message сообщение.
      * @param cause причина возникновения.
      *
      * @since 1.0.0-RC1
      */
-    @Contract(pure = true)
+    @Contract("?, ? -> new")
     public ValidationFault(final @Nullable String message, final @Nullable Throwable cause) {
         super(message, cause, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
     }
 
     /**
-     * Представляет собой конструктор, позволяющий задать параметры подавления и трассировки стека.
+     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением} и {@linkplain #DEFAULT_CAUSE причиной возникновения}
+     * по умолчанию, а также с переданными параметрами подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param stackTrace параметр трассировки стека.
      *
      * @since 1.0.0-RC1
      */
-    @Contract(pure = true)
+    @Contract("?, ? -> new")
     public ValidationFault(final boolean suppression, final boolean stackTrace) {
         super(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, stackTrace);
     }
 
     /**
-     * Представляет собой конструктор, позволяющий задать сообщение, причину возникновения, параметры подавления и
-     * трассировки стека.
+     * Создаёт экземпляр с переданным сообщением, причиной возникновения, параметрами подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина возникновения.
@@ -113,7 +121,7 @@ public class ValidationFault extends UncheckedFault {
      *
      * @since 1.0.0-RC1
      */
-    @Contract(pure = true)
+    @Contract("?, ?, ?, ? -> new")
     public ValidationFault(final @Nullable String message, final @Nullable Throwable cause, final boolean suppression,
                            final boolean stackTrace) {
         super(message, cause, suppression, stackTrace);
