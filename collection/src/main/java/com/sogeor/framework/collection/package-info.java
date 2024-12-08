fe06267619570b960a3f3dff822d792467efab13
George <bloogefest@gmail.com>
2024-12-09 00:08:46 +0300

    Изменение пакета `com.sogeor.framework.collection`

--- collection/src/main/java/com/sogeor/framework/collection/package-info.java
+++ collection/src/main/java/com/sogeor/framework/collection/package-info.java
@@ -15,16 +15,16 @@
  */
 
 /**
- * Представляет собой пакет для работы с неизменяемыми, унаследованными и изменяемыми коллекциями, а также с коллекциями
- * только для чтения и только для записи.
+ * Предоставляет фундаментальные коллекции.
  *
- * @see com.sogeor.collection.immutable
- * @see com.sogeor.collection.inherited
- * @see com.sogeor.collection.mutable
- * @see com.sogeor.collection.readable
- * @see com.sogeor.collection.writable
- * @see com.sogeor.collection.Collection
- * @see com.sogeor.collection.CollectionFault
+ * @see com.sogeor.framework.collection.immutable
+ * @see com.sogeor.framework.collection.inherited
+ * @see com.sogeor.framework.collection.mutable
+ * @see com.sogeor.framework.collection.readable
+ * @see com.sogeor.framework.collection.writable
+ * @see com.sogeor.framework.collection.Collection
+ * @see com.sogeor.framework.collection.CollectionFault
+ * @see com.sogeor.framework.collection.Iterator
  * @since 1.0.0-RC1
  */
-package com.sogeor.collection;
+package com.sogeor.framework.collection;
