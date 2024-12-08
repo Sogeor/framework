1b35803c960e9a40f7de4054144d5bf2c3973008
George <bloogefest@gmail.com>
2024-12-08 17:09:11 +0300

    Введение неисправности `NonEqualValidationFault`

---
+++ validation/src/main/java/com/sogeor/validation/NonEqualValidationFault.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2024 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.validation;
+
+import com.sogeor.annotation.Contract;
+import com.sogeor.annotation.NonNull;
+import com.sogeor.annotation.Nullable;
+
+/**
+ * Представляет собой непроверяемую программную неисправность, связанную с неудачной валидацией равенства объектов.
+ *
+ * @since 1.0.0-RC1
+ */
+public class NonEqualValidationFault extends ValidationFault {
+
+    /**
+     * Содержит сообщение для объектов по умолчанию.
+     *
+     * @since 1.0.0-RC1
+     */
+    public static final @NonNull String DEFAULT_OBJECTS_MESSAGE = "The objects must be equal";
+
+    /**
+     * Содержит сообщение для значений по умолчанию.
+     *
+     * @since 1.0.0-RC1
+     */
+    public static final @NonNull String DEFAULT_VALUES_MESSAGE = "The values must be equal";
+
+    /**
+     * Содержит шаблонное сообщение.
+     *
+     * @since 1.0.0-RC1
+     */
+    public static final @NonNull String TEMPLATE_MESSAGE = "%s must be equal";
+
+    /**
+     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, {@linkplain #DEFAULT_CAUSE причиной возникновения},
+     * параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по
+     * умолчанию.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("-> new")
+    public NonEqualValidationFault() {
+        super(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
+    }
+
+    /**
+     * Создаёт экземпляр с {@linkplain #DEFAULT_CAUSE причиной возникновения}, параметрами
+     * {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию,
+     * а также с переданным сообщением.
+     *
+     * @param message сообщение.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("? -> new")
+    public NonEqualValidationFault(final @Nullable String message) {
+        super(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
+    }
+
+    /**
+     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, параметрами
+     * {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию,
+     * а также с переданной причиной возникновения.
+     *
+     * @param cause причина возникновения.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("? -> new")
+    public NonEqualValidationFault(final @Nullable Throwable cause) {
+        super(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
+    }
+
+    /**
+     * Создаёт экземпляр с параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и
+     * {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию, а также с переданным сообщением и причиной
+     * возникновения.
+     *
+     * @param message сообщение.
+     * @param cause причина возникновения.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("?, ? -> new")
+    public NonEqualValidationFault(final @Nullable String message, final @Nullable Throwable cause) {
+        super(message, cause, DEFAULT_SUPPRESSION, DEFAULT_STACK_TRACE);
+    }
+
+    /**
+     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением} и {@linkplain #DEFAULT_CAUSE причиной возникновения}
+     * по умолчанию, а также с переданными параметрами подавления и трассировки стека.
+     *
+     * @param suppression параметр подавления.
+     * @param stackTrace параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("?, ? -> new")
+    public NonEqualValidationFault(final boolean suppression, final boolean stackTrace) {
+        super(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, stackTrace);
+    }
+
+    /**
+     * Создаёт экземпляр с переданным сообщением, причиной возникновения, параметрами подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param cause причина возникновения.
+     * @param suppression параметр подавления.
+     * @param stackTrace параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("?, ?, ?, ? -> new")
+    public NonEqualValidationFault(final @Nullable String message, final @Nullable Throwable cause,
+                                   final boolean suppression, final boolean stackTrace) {
+        super(message, cause, suppression, stackTrace);
+    }
+
+}
