363b07b225a55ae2a3cf0be88a1acfdd3b5fc2fc
George <bloogefest@gmail.com>
2024-12-08 20:10:41 +0300

    Изменение файла `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,6 @@ subprojects {
             options.encoding = "UTF-8"
         }
 
-        tasks.compileTestJava {
-            options.encoding = "UTF-8"
-        }
-
         tasks.javadoc {
             options.encoding = "UTF-8"
             val options = options as CoreJavadocOptions
@@ -37,6 +33,21 @@ subprojects {
             options.addStringOption("tag", "apiNote:a:\"API Note:\"")
             options.addStringOption("tag", "implSpec:a:\"Implementation Requirements:\"")
             options.addStringOption("tag", "implNote:a:\"Implementation Note:\"")
+        }
+
+        if (name != "annotation") {
+            dependencies {
+                testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+                testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+            }
+
+            tasks.compileTestJava {
+                options.encoding = "UTF-8"
+            }
+
+            tasks.test {
+                useJUnitPlatform()
+            }
         }
     }
 }
